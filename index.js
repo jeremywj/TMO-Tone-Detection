77f243d8a1bd16d36d8b86a8c0dcceda996286d1
Jeremy Jefferson <jeremy@jeremyj.com>
2022-06-20 12:30:40 -0900

    Rebranding

--- index.js
+++ index.js
@@ -1,93 +1,34 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const { program } = require('commander');
-setupProgram();
-
-const { toneDetector } = require('./bin/toneDetector');
-const { csvToConfig } = require('./bin/csvToConfig');
-const { fdToneNotify } = require('./bin/fdToneNotify');
+const { DetectionService } = require("./service/DetectionService");
+const config = require("config");
 const log = require('./util/logger');
+const { AudioService } = require('./service/AudioService');
 
-
-function setupProgram() {
-    program
-        .name("fd-tone-notify")
-        .option('--debug', 'Overrides FD_LOG_LEVEL environment var forcing the log level to debug')
-        .option('--silly', 'Overrides FD_LOG_LEVEL environment var forcing the log level to silly')
-        .option('--instance-name', 'Overrides NODE_APP_INSTANCE environment allowing different config files for different instances running' +
-            ' on the same machine. Example: "--instance-name my-fd" will load config files default-my-fd.json and local-my-fd.json')
-        .parse();
-
-    defaultConfig();
-    overrideEnvVars(program.opts());
-    validateOptions(program.opts());
-}
-
-async function main() {
-
-    const options = program.opts();
-
-    if (options.csvToConfig)
-        csvToConfig();
-    else if (options.allToneDetector)
-        toneDetector({ webServer: options.webServer });
-    else if (options.testNotifications)
-        testNotifications();
-    else
-        fdToneNotify({ webServer: options.webServer });
-}
-
-function overrideEnvVars(options) {
-    if (options.debug)
-        process.env.FD_LOG_LEVEL = "debug";
-    if (options.silly)
-        process.env.FD_LOG_LEVEL = "silly";
-
-    if (options.instanceName)
-        process.env.NODE_APP_INSTANCE = options.instanceName;
-
-    if (options.port)
-        process.env.FD_PORT = Number.parseInt(options.port);
-}
-
-function defaultConfig() {
-    if (!fs.existsSync('./config')) {
-        console.log('No config directory. Initializing with default configuration');
-        fs.mkdirSync('./config');
-
-        // javascript-obfuscator:disable
-        const defaultConfig = require('./config/default.json');
-        fs.writeFileSync('./config/default.json', JSON.stringify(defaultConfig, null, 2));
-
-        //For Packing
-        // javascript-obfuscator:disable
-        const configPath = path.join(__dirname, './config/asound.conf');
-        const data = fs.readFileSync(configPath);
-        fs.writeFileSync('./config/asound.conf', data);
-    }
-}
-
-function validateOptions(options) {
-    const mainOptionSelectedCount = [options.testNotifications, options.allToneDetector].filter(v => !!v).length;
-    if (mainOptionSelectedCount > 1)
-        _exitWithError(`Multi main options selected. Can only selected one of the following: --all-tone-detector, --test-notifications`);
-    if (options.port && !options.webServer)
-        log.warning(`--port <port> option is meaningless without the --web-server option. ` +
-            `Monitoring interface will only start on specified port when --web-server option is set`)
-}
-
-function _exitWithError(message) {
-    log.error(`Cannot run webserver in --test-notification mode`);
-    process.exit(1);
-}
-
-main()
-    .then(r => {
-        log.silly(`Started`);
-        setTimeout(() => log.silly('Main HB'), 10);
-    }
-    )
-    .catch(err => {
-        log.crit(err.stack)
+async function TMOToneDetect({ webServer = false } = {}) {
+    const audioInterface = new AudioService();
+    const detectionService = new DetectionService({
+        audioInterface,
+        silenceAmplitude: config.audio.silenceAmplitude,
+        sampleRate: config.audio.sampleRate,
+        frequencyScaleFactor: config.audio.frequencyScaleFactor,
     });
+    config.detection.detectors.forEach(detectorConfig => {
+        const options = {
+            name: detectorConfig.name,
+            tones: detectorConfig.tones,
+            resetTimeoutMs: detectorConfig.resetTimeoutMs ? detectorConfig.resetTimeoutMs : config.detection.defaultResetTimeoutMs,
+            lockoutTimeoutMs: detectorConfig.lockoutTimeoutMs ? detectorConfig.lockoutTimeoutMs : config.detection.defaultLockoutTimeoutMs,
+            matchThreshold: detectorConfig.matchThreshold ? detectorConfig.matchThreshold : config.detection.defaultMatchThreshold,
+            tolerancePercent: detectorConfig.tolerancePercent ? detectorConfig.tolerancePercent : config.detection.defaultTolerancePercent,
+            notifications: detectorConfig.notifications
+        };
+        log.info(`Adding Detector for ${options.name} with tones ${options.tones.map(v => `${v}Hz`).join(', ')}. `
+            + `Match Threshold: ${options.matchThreshold}, Tolerance: ${options.tolerancePercent * 100}%`);
+        detectionService.addToneDetector(options);
+    });
+
+    audioInterface.start();
+
+    setInterval(() => log.silly("FD Tone Notify Heartbeat"), 60 * 60 * 1000);
+}
+
+TMOToneDetect();
