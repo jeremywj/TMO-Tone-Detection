e78281b8a5e8dab9df1b490076596722e808fef7
Jeremy Jefferson <jeremy@jeremyj.com>
2022-06-20 11:04:14 -0900

    2nd Cleanup

--- notifiers/index.js
+++ notifiers/index.js
@@ -16,86 +16,19 @@ async function sendPreRecordingNotifications(notificationParams) {
     }
 
     log.debug(`Processing PRE recording notifications for ${notificationParams.detector.name}. UUID: ${notificationParams.uuid}`);
-    const p = _pushbulletNotifications(notificationParams, PRE);
-    let promises = [p];
+    let promises = [];
     if (notificationParams.notifications.preRecording) {
-        promises = promises.concat(_emailNotifications(notificationParams, PRE));
         promises = promises.concat(_webhooks(notificationParams, PRE));
         promises = promises.concat(_externalCommands(notificationParams, PRE));
     }
     return Promise.allSettled(promises);
 }
 
-async function sendPostRecordingNotifications(notificationParams) {
-    if (!(notificationParams instanceof NotificationParams)) {
-        const message = "notificationParams must be instance of NotificationParams";
-        log.error(message);
-        throw new Error(message);
-    }
 
-    log.debug(`Processing POST recording notifications ${notificationParams.detector.name}. UUID: ${notificationParams.uuid}`);
-    const p = _pushbulletNotifications(notificationParams, POST);
-    let promises = [p];
-    if (notificationParams.notifications.postRecording) {
-        promises = promises.concat(_emailNotifications(notificationParams, POST));
-        promises = promises.concat(_webhooks(notificationParams, POST));
-        promises = promises.concat(_externalCommands(notificationParams, POST));
-    }
-    return Promise.allSettled(promises);
-}
 
-async function _pushbulletNotifications(params, prePostType) {
-    const pushbullets = params.getPushbullets(prePostType);
-    if (pushbullets.length === 0) {
-        log.debug(`No Pushbullet ${prePostType} Recording notifications`);
-        return;
-    }
 
-    log.info(`Sending ${pushbullets.length}x ${prePostType} Recording Pushbullet notifications`);
-    return pushbullets.map(push => {
-        if (prePostType === PRE) {
-            const options = new PushMessageOptions({ ...push, timestamp: params.timestamp, isTest: params.isTest })
-            return PushBulletService.push(options)
-                .then(r => log.info(chalk.bold.green("Initial Push Sent")))
-                .catch(err => {
-                    log.error("Error sending Initial push");
-                    log.debug(err.stack);
-                });
-        } else {
-            const options = {
-                filename: params.filename,
-                title: `${params.detector ? params.detector.name : ''} Dispatch Audio`,
-                absolutePath: path.resolve("./", params.filename),
-                channelTag: push.channelTag,
-            };
 
-            return PushBulletService.pushFile(options)
-                .then(r => log.info("Push Audio Link Sent"))
-                .catch(err => {
-                    log.error("Error Audio Link Push");
-                    log.debug(err.stack);
-                });
-        }
-    });
-}
 
-async function _emailNotifications(params, prePostType) {
-    const emails = params.getEmails(prePostType);
-    if (emails.length === 0)
-        return;
-
-    log.info(`Sending ${emails.length}x ${prePostType} Recording email notifications`);
-    return emails.map(email => {
-        return sendEmail({
-            ...email, filename: params.filename, isTest: params.isTest,
-            recordingRelPath: `./${params.filename}`, attachFile: params.attachFile
-        })
-            .catch(err => {
-                log.error(`Email Error: ${emails.to}`);
-                log.debug(err.stack);
-            })
-    })
-}
 
 async function _webhooks(params, prePostType) {
     const webhooks = params.getWebhooks(prePostType);
@@ -155,4 +88,4 @@ async function _externalCommands(params, prePostType) {
     })
 }
 
-module.exports = { sendPreRecordingNotifications, sendPostRecordingNotifications };
+module.exports = { sendPreRecordingNotifications };
--- test/notifiers/bin/test.external.js
+++
@@ -1,9 +0,0 @@
-const myArgs = process.argv.slice(2);
-
-function main(){
-    console.log('myArgs: ', myArgs);
-    if(myArgs[0] === "0")
-        process.exit(1);
-}
-
-main();
