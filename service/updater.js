fdf22c00ece6b53d8982b64bc6d181fce7766308
Jeremy Jefferson <jeremy@jeremyj.com>
2023-07-10 15:19:37 -0400

    1.02

--- service/updater.js
+++ service/updater.js
@@ -1,23 +1,21 @@
 const AutoGitUpdate = require('auto-git-update');
 const config = require("config");
 
-const updateConfig = {
-    repository: 'https://github.com/jeremywj/TMO-Tone-Detection',
-    fromReleases: true,
-    tempLocation: config.updateTmpPath,
-    ignoreFiles: [],
-    executeOnComplete: 'pm2 restart TMO',
-    exitOnComplete: false
-}
-
-const updater = new AutoGitUpdate(updateConfig);
-
 var int = 60 //minutes
 
-int = 60 * 60 * 1000;
+int = int * 60 * 1000;
 
 function update() {
     if (config.autoUpdate) {
+        const updateConfig = {
+            repository: 'https://github.com/jeremywj/TMO-Tone-Detection',
+            fromReleases: true,
+            tempLocation: config.updateTmpPath,
+            ignoreFiles: [],
+            executeOnComplete: 'pm2 restart TMO',
+            exitOnComplete: false
+        }
+        const updater = new AutoGitUpdate(updateConfig);
         updater.autoUpdate();
     }
 }
