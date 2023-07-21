82f57b2ccf1fe0f6972494201e78a055c863c4ba
Jeremy Jefferson <jeremy@jeremyj.com>
2023-07-21 13:34:38 -0400

    Fix mic EventEmitter leak

--- service/AudioService.js
+++ service/AudioService.js
@@ -2,16 +2,21 @@ const mic = require('mic');
 const config = require("config");
 const log = require('../util/logger');
 const NO_DATA_INTERVAL_SEC = 30;
+
 class AudioService {
     constructor() {
         this._dataListenerCallbacks = [];
+
         this._setupMic();
+
         this.listenForMicInputEvents();
         this._setNoDataInterval();
     }
+
     start() {
         this._micInstance.start();
     }
+
     restart() {
         log.warning('Restarting mic instance');
         this._micInstance.stop();
@@ -21,6 +26,7 @@ class AudioService {
 
         this.start();
     }
+
     _setupMic() {
         this._micInstance = mic({
             rate: `${config.audio.sampleRate}`,
@@ -31,31 +37,36 @@ class AudioService {
         });
         this._micInputStream = this._micInstance.getAudioStream();
     }
+
     _resetNoDataInterval() {
         clearInterval(this._noDataInterval);
         this._setNoDataInterval();
     }
+
     _setNoDataInterval() {
         this._noDataInterval = setInterval(() => {
             log.alert(`No Mic Data for ${NO_DATA_INTERVAL_SEC} Seconds`);
             this.restart();
         }, NO_DATA_INTERVAL_SEC * 1000);
     }
-    onData(callback) {
 
+    onData(callback) {
         this._dataListenerCallbacks.push(callback);
         this._micInputStream.on('data', callback);
     }
+
     _rebindDataListenerCallbacks() {
         this._dataListenerCallbacks.forEach(cb => this._micInputStream.on('data', cb));
     }
+
     listenForMicInputEvents() {
-        this.onData(() => {
+        this._micInputStream.on('data', () => {
             this._resetNoDataInterval();
         });
         listenForMicInputEvents(this._micInputStream);
     }
 }
+
 function listenForMicInputEvents(micInputStream) {
     micInputStream.on('error', function (err) {
         log.alert("Error in Mic Input Stream: " + err);
@@ -78,4 +89,6 @@ function listenForMicInputEvents(micInputStream) {
         log.alert("Mic Instance Has resumed");
     });
 }
+
+
 module.exports = { AudioService, listenForMicInputEvents };
